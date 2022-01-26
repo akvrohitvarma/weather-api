5c16da60debb1ba3b7c2364968857647af3c50c0
Rohit Varma <rohitvarmaalluri009@gmail.com>
2022-01-26 18:32:13 +0530

    removed uneccesary files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { WeatherAppComponent } from './weather-app/weather-app.component';
 import { HttpClientModule } from '@angular/common/http';
 
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    WeatherAppComponent
+    AppComponent
   ],
   imports: [
     BrowserModule, HttpClientModule, FormsModule
