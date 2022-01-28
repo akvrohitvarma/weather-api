e1b3cf47db045413a83d1417e6525b421a755144
Rohit Varma <rohitvarmaalluri009@gmail.com>
2022-01-28 09:44:21 +0530

    Added error handling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,6 +18,7 @@ export class AppComponent {
   max_temp="";
   pressure="";
   temps:any;
+  error:any;
   constructor(private wdata:WeatherDataService){
     this.wdata.getdata("Visakhapatnam").subscribe(data=>{
       console.log("data ",data);
@@ -29,7 +30,10 @@ export class AppComponent {
       this.min_temp = obj.main.temp_min;
       this.max_temp = obj.main.temp_max;
       this.pressure = obj.main.pressure;
-    });
+    },(error) => {
+      console.log(error)
+      this.error = error
+    })
   }
 
   ongo(){
@@ -43,10 +47,10 @@ export class AppComponent {
       this.min_temp = obj.main.temp_min;
       this.max_temp = obj.main.temp_max;
       this.pressure = obj.main.pressure;
-    });
+    },(error) => {
+      console.log(error)
+      alert("The City which you entered does not exist!!")
+      this.error = error
+    })
   }
 }
-
-/*
-{"coord":{"lon":81.7833,"lat":16.9833},"weather":[{"id":721,"main":"Haze","description":"haze","icon":"50d"}],"base":"stations","main":{"temp":27.13,"feels_like":28.32,"temp_min":27.13,"temp_max":27.13,"pressure":1013,"humidity":61},"visibility":5000,"wind":{"speed":3.09,"deg":100},"clouds":{"all":20},"dt":1643175630,"sys":{"type":1,"id":9225,"country":"IN","sunrise":1643159121,"sunset":1643199902},"timezone":19800,"id":1258932,"name":"Rajahmundry","cod":200}
-*/
--- src/app/services/weather-data.service.ts
+++ src/app/services/weather-data.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { HttpErrorResponse } from '@angular/common/http'
+import { catchError, Observable, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -7,7 +9,12 @@ import { HttpClient } from '@angular/common/http';
 export class WeatherDataService {
 
   constructor(public data:HttpClient) { }
-  getdata(cityname:string){
-    return this.data.get('https://api.openweathermap.org/data/2.5/weather?q='+cityname+'&units=metric&appid=284a9239a736c79386472332a4de0628')
+  getdata(cityname:string):Observable<any>{
+    var url = 'https://api.openweathermap.org/data/2.5/weather?q='+cityname+'&units=metric&appid=284a9239a736c79386472332a4de0628'
+    return this.data.get<any>(url).pipe(catchError(this.handleError))
+  }
+
+  handleError(error:any){
+      return throwError(error.message || "City not found!!")
   }
 }
